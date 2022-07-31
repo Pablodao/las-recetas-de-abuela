c36497d3157e54ca79d51df1724a92a4a343c074
Pablo de Andres <pablodeandresolano@gmail.com>
2022-07-31 19:27:47 +0200

    Create GET router for recipe list

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
+const Recipe = require("../models/Recipe.model.js");
+//GET "/recipes" => Render a view with all the recipes
+router.get("/", async (req, res, next) => {
+  try {
+    const recipeList = await Recipe.find().select(
+      "name, image, difficulty, category"
+    );
+    res.render("recipes/list.hbs", { recipeList });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//GET "/recipes/create" => Render create recipe form view
+//POST "/recipes/create" => Creates a recipe in the DB and redirect
+
+//GET "/recipes/:recipeId" => Render view with all the recipe details
+
+//GET "/recipes/:recipeId/edit" => Render edit recipe form view
+//POST "/:recipeId/edit" => Edit a recipe and redirect
+
+//POST "/recipes/:recipeId/delete" => Delete a recipe from the DB
 
 module.exports = router;
