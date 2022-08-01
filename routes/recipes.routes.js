0e4e17a6e59e1b5f22518210a1537b408431596f
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-01 10:52:05 +0200

    finished routes for create new recipe

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Recipe = require("../models/Recipe.model.js");
+
+
 //GET "/recipes" => Render a view with all the recipes
 router.get("/", async (req, res, next) => {
   try {
@@ -13,7 +15,34 @@ router.get("/", async (req, res, next) => {
 });
 
 //GET "/recipes/create" => Render create recipe form view
+router.get("/create", (req, res, next) => {
+  res.render("recipes/new-recipe.hbs");
+});
 //POST "/recipes/create" => Creates a recipe in the DB and redirect
+router.post("/create", async (req, res, next) => {
+  const { name, instructions, image, ingredients, difficulty, category } =
+    req.body;
+  try {
+    await Recipe.create({
+      name,
+      instructions,
+      image,
+      ingredients,
+      difficulty,
+      category,
+    });
+    req.redirect("./recipes");
+  } catch (err) {
+    next(err);
+  }
+});
+
+//POST "/recipes/ingredients" Adds a ingredient to the ingredients list of a recipe and redirect
+router.post("/ingredient", async (req, res, next) => {
+
+    {name}
+
+})
 
 //GET "/recipes/:recipeId" => Render view with all the recipe details
 
