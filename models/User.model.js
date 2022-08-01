f9eb87c29987e560982a2ba10635daa397d72cde
Malva Prior <malvacs@gmail.com>
2022-08-01 16:19:50 +0200

    avatar

--- models/User.model.js
+++ models/User.model.js
@@ -9,7 +9,8 @@ const userSchema = new Schema(
     },
     avatar: {
       type: String,
-      default:"/images/avatar2.jpg"  
+      enum:["/images/avatar1.jpg", "/images/avatar2.jpg", "/images/avatar3.jpg", "/images/avatar4.jpg"],
+      default:"/images/avatar1.jpg"  
     },
     password: {
       type: String,
