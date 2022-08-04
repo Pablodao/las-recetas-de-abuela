fcf2c2cf4a936de0f6d2020cb84c1889ed24b474
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-04 10:48:56 +0200

    Changes error  messages in login and signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const User = require("../models/User.model.js");
 const bcrypt = require("bcryptjs");
 
-
 //* SIGNUP
 
 // GET "/auth/signup" => Render signup view
@@ -13,20 +12,46 @@ router.get("/signup", (req, res, next) => {
 // POST "/auth/signup" => Creates the user in the DB and redirect
 router.post("/signup", async (req, res, next) => {
   const { username, email, password } = req.body;
-  console.log("REQBODY SIGNUP",req.body)
+  console.log("REQBODY SIGNUP", req.body);
   //* GC
   // Empty input
-  if (username === "" || email === "" || password === "") {
+  if (
+    (username === "") & (email === "") & (password === "") ||
+    (username === "") & (email === "") ||
+    (username === "") & (password === "") ||
+    (email === "") & (password === "")
+  ) {
     res.render("auth/signup.hbs", {
       errorMessage: "Por favor rellene todos los campos",
     });
     return;
   }
+
+  if (username === "") {
+    res.render("auth/signup.hbs", {
+      usernameErrorMessage: "Por favor rellene el campo del nombre de usuario",
+    });
+    return;
+  }
+  if (email === "") {
+    res.render("auth/signup.hbs", {
+      emailErrorMessage: "Por favor rellene el campo del email",
+    });
+    return;
+  }
+
+  if (password === "") {
+    res.render("auth/signup.hbs", {
+      passwordErrorMessage: "Por favor rellene el campo de la contraseña",
+    });
+    return;
+  }
+
   // Strong password
   const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/;
   if (passwordRegex.test(password) === false) {
     res.render("auth/signup.hbs", {
-      errorMessage:
+      passwordErrorMessage:
         "La contraseña debe contener mínimo una mayúscula, una minúscula, un número, un carácter especial y entre 8 y 64 caracteres",
     });
     return;
@@ -37,24 +62,24 @@ router.post("/signup", async (req, res, next) => {
     /^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/;
   if (emailRegex.test(email) === false) {
     res.render("auth/signup.hbs", {
-      errorMessage: "Por favor, introduzca una dirección de correo válida",
+      emailErrorMessage: "Por favor, introduzca una dirección de correo válida",
     });
     return;
   }
 
   try {
-    // GC User already exist
+    // GC Email already exist
     const foundUser = await User.findOne({ email });
     if (foundUser !== null) {
       res.render("./auth/signup.hbs", {
-        errorMessage: "Correo electrónico en uso",
+        emailErrorMessage: "Correo electrónico en uso",
       });
     }
     // GC Username already in use
     const foundUserByUsername = await User.findOne({ username });
     if (foundUserByUsername !== null) {
       res.render("./auth/signup.hbs", {
-        errorMessage: "El nombre de usuario ya se encuentra en uso",
+        usernameErrorMessage: "El nombre de usuario ya se encuentra en uso",
       });
     }
     console.log("TODAS LAS VALIDACIONES HAN SIDO SUPERADAS!");
@@ -86,14 +111,20 @@ router.get("/login", (req, res, next) => {
 router.post("/login", async (req, res, next) => {
   const { access, password } = req.body;
   //Empty input
-  console.log("REQ.BODY",req.body)
-  if (access === "" || password === "") {
+  console.log("REQ.BODY", req.body);
+  if (access === "") {
     res.render("./auth/login.hbs", {
-      errorMessage: "Por favor, rellene todos los campos",
+      errorMessage: "Por favor, introduzca su nombre de usuario o email",
     });
     return;
   }
-  console.log("Fuera del try")
+
+  if (password === "") {
+    res.render("./auth/login.hbs", {
+      passwordErrorMessage: "Contraseña requerida",
+    });
+    return;
+  }
   try {
     //Find user
     const foundUser = await User.findOne({
@@ -103,22 +134,24 @@ router.post("/login", async (req, res, next) => {
       res.render("./auth/login.hbs", { errorMessage: "Usuario no encontrado" });
       return;
     }
-   
+
     //valid password
-    const isPasswordValid = await bcrypt.compare(password, foundUser.password)
-    console.log(password)
+    const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+    console.log(password);
     if (isPasswordValid === false) {
-      res.render("./auth/login.hbs", { errorMessage: "Constraseña inválida" });
+      res.render("./auth/login.hbs", {
+        passwordErrorMessage: "Constraseña inválida",
+      });
       return;
     }
-    console.log("Dentro del try")
+    console.log("Dentro del try");
     req.session.user = {
       _id: foundUser._id,
       email: foundUser.email,
       username: foundUser.username,
     };
     req.session.save(() => {
-       res.redirect("/");
+      res.redirect("/");
     });
   } catch (err) {
     next(err);
@@ -127,12 +160,10 @@ router.post("/login", async (req, res, next) => {
 
 //* LOG OUT
 //GET "/auth/logout" => user can log out
-router.get("/logout", (req, res, next) =>{
-  req.session.destroy(() =>{
-    res.redirect("/")
-  })
-})
-
-
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
